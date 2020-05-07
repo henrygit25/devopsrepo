68dbd4122d32a0c20061168218cbae28f14a12d3
henrygit25 <64952458+henrygit25@users.noreply.github.com>
2020-05-07 15:40:19 +0530

    helloworld
    helloworld

---
+++ Helloworld.java
@@ -0,0 +1,14 @@
+package com.infy.java.basic;
+
+public class Helloworld {
+	
+	public Helloworld() {
+		System.out.println("Hello world");
+	}
+
+	public static void main(String[] args) {
+		new Helloworld();
+
+	}
+
+}
