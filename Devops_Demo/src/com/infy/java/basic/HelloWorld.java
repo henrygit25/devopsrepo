8aeecca214f232f227e1c5a45e68db50f70c1e6b
henrygit25 <64952458+henrygit25@users.noreply.github.com>
2020-05-07 16:11:11 +0530

    Intital Commit.

---
+++ Devops_Demo/src/com/infy/java/basic/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.infy.java.basic;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		System.out.println("Hello world");
+		System.out.println("GIT in eclipse");
+	}
+
+	public static void main(String[] args) {
+		new HelloWorld();
+
+	}
+
+}
